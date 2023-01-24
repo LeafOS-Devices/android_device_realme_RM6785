bceb75b4c2b336629e9187947a12af1e2b26315e
SamarV-121 <samarvispute121@pm.me>
2023-01-24 16:29:47 +0530

    RM6785: vibrator: Fix amplitude control
     * Our sysfs node expects value between 0-9 while setAmplitude() reports values between 0.0f-1.0f
       multiply received amplitude by max intensity
    
    Change-Id: I77232171c0d647979191d957187ae783c92048a1

--- aidl/vibrator/Vibrator.h
+++ aidl/vibrator/Vibrator.h
@@ -12,6 +12,12 @@
 #define VIBRATOR_DURATION "/sys/class/leds/vibrator/duration"
 #define VIBRATOR_ACTIVATE "/sys/class/leds/vibrator/activate"
 #define VIBRATOR_INTENSITY "/sys/kernel/thunderquake_engine/level"
+#define INTENSITY_MIN 0
+#define INTENSITY_MAX 9
+
+#define INTENSITY_LIGHT 0.23
+#define INTENSITY_MEDIUM 0.56
+#define INTENSITY_STRONG 1
 
 using ::aidl::android::hardware::vibrator::IVibratorCallback;
 using ::aidl::android::hardware::vibrator::Braking;
